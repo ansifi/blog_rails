b9e466b1b17a90fb1e55fd9400270d0fda6fcc81
Ansif <ansif.pi@csrinformatik.de>
2023-09-01 14:50:00 +0530

    Micro reddit code added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  root "articles#index"
+  devise_for :users
+  resources :comments
+  resources :posts
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :articles do
-    resources :comments
-  end
+  # Defines the root path route ("/")
+  root "posts#index"
 end
